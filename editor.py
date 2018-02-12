35749397424338d44a64cf223623869156493cdc
Sebastian Krzyszkowiak <dos@dosowisko.net>
2018-02-12 02:49:40 +0100

    ability to add objects to scene

--- editor.py
+++ editor.py
@@ -4,6 +4,7 @@
 import yaml
 import sys
 import signal
+import math
 from os import listdir
 from os.path import isfile, join
 from PyQt5.QtCore import *
@@ -15,10 +16,72 @@ app = QApplication(sys.argv)
 
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-#imagecache = {}
+def hasSomePixel(image, x1, y1, x2, y2):
+  if x1 < 0 or x2 < 0 or y1 < 0 or y2 < 0:
+    return False
+  w, h = image.width(), image.height()
+  if x1 >= w or x2 >= w or y2 >= h or y1 >= h:
+    return False
 
-#frameNr = 0
+  for x in range(x1, x2 + 1):
+    for y in range(y1, y2 + 1):
+      #print(x, y, image.pixelColor(x, y).alpha())
+      if image.pixelColor(x, y).alpha() > 0:
+        return True
+  return False  
 
+class SceneElement(QGraphicsPixmapItem):
+    plane = None
+    
+    def  __init__(self, pixmap, x, y, plane):
+        super(QGraphicsPixmapItem, self).__init__(pixmap, parent=plane)
+        self.setFlags(QGraphicsItem.ItemIsSelectable | QGraphicsItem.ItemIsMovable)
+        self.plane = plane.plane
+        self.setPos(x, y)
+
+class SourceItem(QGraphicsPixmapItem):
+        
+    def mousePressEvent(self, event):
+        x = math.floor(event.pos().x())
+        y = math.floor(event.pos().y())
+        image = self.pixmap().toImage()
+        
+        
+        x1 = x
+        y1 = y
+        x2 = x
+        y2 = y
+        
+        allClear = False
+
+        while not allClear:
+          allClear = True
+          while hasSomePixel(image, x2 + 1, y1, x2 + 1, y2):
+            allClear = False
+            x2 = x2 + 1
+          while hasSomePixel(image, x1, y2 + 1, x2, y2 + 1):
+            allClear = False
+            y2 = y2 + 1
+          while hasSomePixel(image, x1 - 1, y1, x1 - 1, y2):
+            allClear = False
+            x1 = x1 - 1
+          while hasSomePixel(image, x1, y1 - 1, x2, y1 - 1):
+            allClear = False
+            y1 = y1 - 1
+          #print("done pass", x1,y1,x2-x1+1,y2-y1+1)
+        
+        print(x1,y1,x2-x1+1,y2-y1+1)
+        
+        lastFileSelection.setRect(QRectF(x1, y1, x2 - x1 + 1, y2 - y1 + 1))
+        plane = activePlane
+        if plane == None:
+          plane = x1 // 2400
+          #setActivePlane(plane)
+        scene.addItem(SceneElement(self.pixmap().copy(x1, y1, x2 - x1 + 1, y2 - y1 + 1), x1, y1, planes[plane]))
+        
+        
+        #print(self.pixmap().toImage().pixelColor(event.pos().x(), event.pos().y()))
+        return super(SourceItem, self).mousePressEvent(event)
 
 class Window(QMainWindow):
     resized = pyqtSignal()
@@ -43,16 +106,21 @@ def selectionChanged():
   items = scene.selectedItems()
   if len(items):
     setActivePlane(items[0].plane)
+  else:
+    setActivePlane(None)
 
 def renderFile(name):
+  global lastFileSelection
   fileScene.clear()
   fileScene.addRect(QRectF(-20000, -20000, 40000, 40000), QPen(QColor("transparent")), QBrush(QColor("lightgrey")))
   checkerboard = fileScene.addRect(QRectF(-20000, -20000, 40000, 40000), QPen(QColor("transparent")), QBrush(QColor("grey"), Qt.Dense3Pattern))
   checkerboard.setFlags(QGraphicsItem.ItemIgnoresTransformations)
 
   f = QPixmap.fromImage(QImage("frozen/" + name))
-  fileScene.addPixmap(f)
+  fileScene.addItem(SourceItem(f))
   fileScene.setSceneRect(QRectF(f.rect()))
+  
+  lastFileSelection = fileScene.addRect(QRectF(0, 0, 0, 0), QPen(QColor("red")), QBrush(QColor(0,0,255, 64)))
 
   fileView.setScene(fileScene)
   fileView.fitInView(fileScene.sceneRect(), Qt.KeepAspectRatio)
@@ -95,7 +163,6 @@ planes = []
 
 
 planes = [scene.addRect(QRectF(240+2400*i, 135, 1920, 1080), QPen(QColor("white"), 20)) for i in range(4)];
-setActivePlane(0)
 
 for i in range(4):
   planes[i].setFlags(QGraphicsItem.ItemIsSelectable)
@@ -124,7 +191,7 @@ vbox.addWidget(tabs)
 fileView = QGraphicsView()
 
 fileScene = QGraphicsScene()
-
+lastFileSelection = None
 renderFile(files[0])
 
 vbox.addWidget(fileView)
