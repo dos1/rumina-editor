49f140192ff7f28a136f3cfd63f79bf256b0ab28
Sebastian Krzyszkowiak <dos@dosowisko.net>
2018-02-15 03:01:01 +0100

    editor PoC

--- editor.py
+++ editor.py
@@ -11,10 +11,54 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtMultimedia import *
+from pypacker import pack_images, sort_images_by_size
 
 app = QApplication(sys.argv)
 
 signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+lastPlane = -1
+
+results = []
+
+class RuminaSpriteSheet:
+    def __init__(self, w, h):
+        self.w = w
+        self.h = h
+        self.pixmap = QPixmap(QSize(w,h))
+        self.pixmap.fill(QColor('transparent'))
+        self.canvas = QPainter(self.pixmap)
+
+    def paste(self, image, pos):
+        global lastPlane
+        #print(image.sprite, pos)
+        #if lastPlane != image.sprite.plane:
+        #  print('SwitchPlane(l, cam, %d);' % image.sprite.plane)
+        #  lastPlane = image.sprite.plane
+          
+        #print('DrawScenePart(game, data->sceneBitmap, %d, %d, %d, %d, l, cam, %d, %d); // %s' % (pos[0], pos[1], image.width(), image.height(), image.sprite.x, image.sprite.y, image.sprite.sprite_name))
+        results.append((pos[0], pos[1], image.width(), image.height(), image.sprite.x - (2400 * image.sprite.plane), image.sprite.y, image.sprite.plane, image.sprite.index, image.sprite.sprite_name))
+        self.canvas.drawImage(pos[0], pos[1], image)
+
+class RuminaSprite:
+    def __init__(self, name, element, x, y, plane, file_index):
+        self.sprite_name = name
+        self.index = file_index
+        self.image = element.getImage()
+        self.element = element
+        self.image.sprite = self
+        self.padding = 10
+        self.x = x
+        self.y = y
+        self.plane = plane
+
+    def __repr__(self):
+        return '<RuminaSprite %s at %d,%d; %dx%d +%d on plane %d>' % (self.sprite_name, self.x, self.y, self.image.width(), self.image.height(), self.padding, self.plane)
+
+    def get_size(self):
+        (w,h) = self.image.width(), self.image.height();
+        return (self.image.width() + 2*self.padding, self.image.height() + 2*self.padding)
+
 
 def hasSomePixel(image, x1, y1, x2, y2):
   if x1 < 0 or x2 < 0 or y1 < 0 or y2 < 0:
@@ -33,11 +77,19 @@ def hasSomePixel(image, x1, y1, x2, y2):
 class SceneElement(QGraphicsPixmapItem):
     plane = None
     
-    def  __init__(self, pixmap, x, y, plane):
+    def  __init__(self, pixmap, x, y, plane, name):
         super(QGraphicsPixmapItem, self).__init__(pixmap, parent=plane)
         self.setFlags(QGraphicsItem.ItemIsSelectable | QGraphicsItem.ItemIsMovable)
         self.plane = plane.plane
         self.setPos(x, y)
+        self.name = name
+        self.index = currentFileIndex
+                
+    def getSprite(self):
+        return RuminaSprite(self.name, self, self.pos().x(), self.pos().y(), self.plane, self.index)
+    
+    def getImage(self):
+        return self.pixmap().toImage()
 
 class SourceItem(QGraphicsPixmapItem):
         
@@ -59,29 +111,66 @@ class SourceItem(QGraphicsPixmapItem):
           while hasSomePixel(image, x2 + 1, y1, x2 + 1, y2):
             allClear = False
             x2 = x2 + 1
+          print("done pass1", x1,y1,x2-x1+1,y2-y1+1)
           while hasSomePixel(image, x1, y2 + 1, x2, y2 + 1):
             allClear = False
             y2 = y2 + 1
+          print("done pass2", x1,y1,x2-x1+1,y2-y1+1)
           while hasSomePixel(image, x1 - 1, y1, x1 - 1, y2):
             allClear = False
             x1 = x1 - 1
+          print("done pass3", x1,y1,x2-x1+1,y2-y1+1)
           while hasSomePixel(image, x1, y1 - 1, x2, y1 - 1):
             allClear = False
             y1 = y1 - 1
-          #print("done pass", x1,y1,x2-x1+1,y2-y1+1)
+          print("done pass4", x1,y1,x2-x1+1,y2-y1+1)
         
         print(x1,y1,x2-x1+1,y2-y1+1)
+        #print(lastFileSelection.rect().x(), lastFileSelection.rect().y())
+        if lastFileSelection.rect().x() == x1 and lastFileSelection.rect().y() == y1:
+          return super(SourceItem, self).mousePressEvent(event)
         
         lastFileSelection.setRect(QRectF(x1, y1, x2 - x1 + 1, y2 - y1 + 1))
+
         plane = activePlane
         if plane == None:
           plane = x1 // 2400
           #setActivePlane(plane)
-        scene.addItem(SceneElement(self.pixmap().copy(x1, y1, x2 - x1 + 1, y2 - y1 + 1), x1, y1, planes[plane]))
+        SceneElement(self.pixmap().copy(x1, y1, x2 - x1 + 1, y2 - y1 + 1), x1, y1, planes[plane], currentFile)
         
         
         #print(self.pixmap().toImage().pixelColor(event.pos().x(), event.pos().y()))
         return super(SourceItem, self).mousePressEvent(event)
+
+class RuminaGraphicsView(QGraphicsView):
+  def  __init__(self, parent=None):
+    super(QGraphicsView, self).__init__(parent=parent)
+
+  def wheelEvent(self, event):
+    """
+    Zoom in or out of the view.
+    """
+    if not (event.modifiers() & Qt.ControlModifier):
+      return super(QGraphicsView, self).wheelEvent(event)
+    zoomInFactor = 1.0 + 0.001 * event.angleDelta().y()
+    zoomOutFactor = 1 / zoomInFactor
+
+    # Save the scene pos
+    oldPos = self.mapToScene(event.pos())
+
+    # Zoom
+    #if event.angleDelta().y() > 0:
+    zoomFactor = zoomInFactor
+    #else:
+    #    zoomFactor = zoomOutFactor
+    self.scale(zoomFactor, zoomFactor)
+
+    # Get the new position
+    newPos = self.mapToScene(event.pos())
+
+    # Move scene to old position
+    delta = newPos - oldPos
+    self.translate(delta.x(), delta.y())
 
 class Window(QMainWindow):
     resized = pyqtSignal()
@@ -100,7 +189,7 @@ def setActivePlane(plane):
   global activePlane
   activePlane = plane
   for i in range(4):
-      planes[i].setBrush(QBrush(QColor(255, 255, 255, 64 if activePlane == i else 0)))
+      planes[i].setBrush(QBrush(QColor(255, 255, 255, 32 if activePlane == i else 0)))
 
 def selectionChanged():
   items = scene.selectedItems()
@@ -110,9 +199,10 @@ def selectionChanged():
     setActivePlane(None)
 
 def renderFile(name):
-  global lastFileSelection
+  global lastFileSelection, currentFile
   fileScene.clear()
-  fileScene.addRect(QRectF(-20000, -20000, 40000, 40000), QPen(QColor("transparent")), QBrush(QColor("lightgrey")))
+  currentFile = name
+  #fileScene.addRect(QRectF(-20000, -20000, 40000, 40000), QPen(QColor("transparent")), QBrush(QColor("lightgrey")))
   checkerboard = fileScene.addRect(QRectF(-20000, -20000, 40000, 40000), QPen(QColor("transparent")), QBrush(QColor("grey"), Qt.Dense3Pattern))
   checkerboard.setFlags(QGraphicsItem.ItemIgnoresTransformations)
 
@@ -126,7 +216,36 @@ def renderFile(name):
   fileView.fitInView(fileScene.sceneRect(), Qt.KeepAspectRatio)
 
 def fileChanged(index):
+  global currentFileIndex
+  currentFileIndex = index
   renderFile(files[index])
+  
+def doStuff():
+  global lastPlane, results
+  lastPlane = -1
+  items = [ x.getSprite() for x in scene.items() if isinstance(x, SceneElement) ]
+  sorted_items = sort_images_by_size(items)
+  image_packing = pack_images(sorted_items, True, () )
+  print('Packed into size', image_packing.rect.wd, image_packing.rect.hgt)
+  results = []
+  ss = RuminaSpriteSheet(image_packing.rect.wd, image_packing.rect.hgt)
+  image_packing.render(ss)
+  ss.canvas.end()
+  ss.pixmap.save("output.png")
+  #print(results)
+  results_sorted = sorted(results, key = lambda x: x[8])
+  #results_sorted.reverse()
+  #          results.append((pos[0], pos[1], image.width(), image.height(), image.sprite.x, image.sprite.y, image.sprite.sprite_name, image.sprite.plane, image.sprite.index))
+
+  for res in results_sorted:
+    #if lastPlane != res[7]:
+    #  print('SwitchPlane(l, cam, %d);' % res[7])
+    #  lastPlane = res[7]
+          
+    #print(res)
+    print('DrawScenePart(game, data->sceneBitmap, %d, %d, %d, %d, l, cam, %d, %d, %d, %d); // %s' % res)
+
+  print('done')
 
 #def fileChanged(name):
 #  print("Detected changed file", name)
@@ -144,12 +263,12 @@ def fileChanged(index):
 w = Window()
 w.setWindowTitle('Rumina Editor')
 
-view = QGraphicsView()
+view = RuminaGraphicsView()
 bg = QPixmap.fromImage(QImage("bg.webp"))
 scene = QGraphicsScene()
 
 
-scene.addRect(QRectF(-20000, -20000, 40000, 40000), QPen(QColor("transparent")), QBrush(QColor("lightgrey")))
+#scene.addRect(QRectF(-20000, -20000, 40000, 40000), QPen(QColor("transparent")), QBrush(QColor("lightgrey")))
 checkerboard = scene.addRect(QRectF(-20000, -20000, 40000, 40000), QPen(QColor("transparent")), QBrush(QColor("grey"), Qt.Dense3Pattern))
 checkerboard.setFlags(QGraphicsItem.ItemIgnoresTransformations)
 
@@ -159,13 +278,15 @@ scene.setSceneRect(QRectF(bg.rect()))
 scene.selectionChanged.connect(selectionChanged)
 
 activePlane = None
+currentFile = None
+currentFileIndex = None
 planes = []
 
 
 planes = [scene.addRect(QRectF(240+2400*i, 135, 1920, 1080), QPen(QColor("white"), 20)) for i in range(4)];
 
 for i in range(4):
-  planes[i].setFlags(QGraphicsItem.ItemIsSelectable)
+  #planes[i].setFlags(QGraphicsItem.ItemIsSelectable)
   planes[i].plane = i
 
 view.setScene(scene)
@@ -188,11 +309,12 @@ tabs.setDrawBase(False)
 tabs.currentChanged.connect(fileChanged)
 vbox.addWidget(tabs)
 
-fileView = QGraphicsView()
+fileView = RuminaGraphicsView()
 
 fileScene = QGraphicsScene()
 lastFileSelection = None
 renderFile(files[0])
+currentFileIndex = 0
 
 vbox.addWidget(fileView)
 
@@ -201,6 +323,12 @@ widget.setLayout(vbox)
 w.setCentralWidget(widget)
 
 w.resized.connect(onResize)
+
+
+button = QPushButton("do stuff")
+#button.setLabel("do stuff")
+button.clicked.connect(doStuff)
+vbox.addWidget(button)
 #loadFrameData()
 #advanceFrame()
 #QTimer.singleShot(0, advanceFrame)
---
+++ pypacker.py
@@ -0,0 +1,332 @@
+#----------------------------------------------------
+#- pypacker: written by Joe Wezorek
+#- license:  WTFPL
+#- If you use this code and/or have suggestions, etc.,
+#- email me at jwezorek@gmail.com
+
+import os, os.path
+import sys
+import copy
+from PIL import Image, ImageDraw
+from optparse import OptionParser
+from math import log, ceil
+#from metadata_gen import *
+
+def sort_images_by_size(image_files):
+    #sort by area (secondary key)
+    #sorted_images = sorted(image_files, \
+    #        key=lambda sprite: sprite.get_size()[0] * sprite.get_size()[1])
+    #sort by max dimension (primary key)
+    sorted_images = sorted(image_files, \
+           key=lambda img_pair: max(img_pair.get_size()[0], img_pair.get_size()[1]))
+    return sorted_images
+
+#----------------------------------------------------------------------
+
+class sprite_info:
+    def __init__(self, name, img, padding):
+        self.sprite_name = os.path.splitext(name)[0] #strip the extension...
+        self.image = img
+        self.padding = padding
+    def get_size(self):
+        (wd,hgt) = self.image.size;
+        return (wd + 2*self.padding, hgt + 2*self.padding)
+
+
+#----------------------------------------------------------------------
+
+class rectangle:
+    def __init__(self, x=0, y=0, wd=0, hgt=0):
+        self.x = x
+        self.y = y
+        self.wd = wd
+        self.hgt = hgt
+    def split_vert(self,y):
+        top = rectangle(self.x, self.y, self.wd, y)
+        bottom = rectangle(self.x, self.y+y, self.wd, self.hgt-y)
+        return (top, bottom)
+    def split_horz(self,x):
+        left = rectangle(self.x, self.y, x, self.hgt)
+        right = rectangle(self.x+x, self.y, self.wd-x, self.hgt)
+        return (left,right)
+    def area(self):
+        return self.wd * self.hgt
+    def max_side(self):
+        return max(self.wd, self.hgt)
+    def can_contain(self, wd, hgt):
+        return self.wd >= wd and self.hgt >=hgt
+    def is_congruent_with(self, wd, hgt):
+        return self.wd == wd and self.hgt ==hgt
+    def to_string(self):
+        return "<(%d, %d) - (%d, %d)>" % (self.x, self.y, self.wd, self.hgt)
+    def should_split_vertically(self, wd, hgt):
+        if (self.wd == wd):
+            return True
+        elif (self.hgt == hgt):
+            return False
+        #TODO: come up with a better heuristic
+        vert_rects = self.split_vert(hgt)
+        horz_rects = self.split_horz(wd)
+        return vert_rects[1].area() > horz_rects[1].area()
+    def should_grow_vertically(self, wd, hgt):
+        can_grow_vert = self.wd >= wd
+        can_grow_horz = self.hgt >= hgt
+        if (not can_grow_vert and not can_grow_horz):
+            raise Exception("Unable to grow!")
+        if (can_grow_vert and not can_grow_horz):
+            return True
+        if (can_grow_horz and not can_grow_vert):
+            return False
+        return (self.hgt + hgt < self.wd + wd)
+
+
+#----------------------------------------------------------------------
+class rect_node:
+    def __init__(self, sprite, rect=(), children=()):
+        self.rect = rect
+        self.sprite = sprite;
+        self.children = children
+
+    def clone(self):
+        if (self.is_leaf()):
+            return rect_node(copy.copy(self.sprite), copy.copy(self.rect))
+        else:
+            return rect_node(copy.copy(self.sprite), copy.copy(self.rect),\
+                            (self.children[0].clone(), self.children[1].clone()))
+
+    def is_leaf(self):
+        return not self.children
+
+    def is_empty_leaf(self):
+        return (self.is_leaf() and not self.sprite)
+
+    def split_node(self, sprite):
+        if (not self.is_leaf):
+            raise Exception("Attempted to split non-leaf")
+
+        (img_wd, img_hgt) = sprite.get_size()
+        if (not self.rect.can_contain(img_wd, img_hgt)):
+            raise Exception("Attempted to place an img in a node it doesn't fit")
+
+        #if it fits exactly then we are done...
+        if (self.rect.is_congruent_with(img_wd, img_hgt)):
+            self.sprite = sprite
+        else:
+            if (self.rect.should_split_vertically(img_wd, img_hgt)):
+                vert_rects = self.rect.split_vert(img_hgt)
+                top_child = rect_node((), vert_rects[0])
+                bottom_child = rect_node((), vert_rects[1])
+                self.children = (top_child, bottom_child)
+            else:
+                horz_rects = self.rect.split_horz(img_wd)
+                left_child = rect_node((), horz_rects[0])
+                right_child = rect_node((), horz_rects[1])
+                self.children = (left_child, right_child)
+            self.children[0].split_node(sprite)
+
+    def grow_node(self, sprite):
+        if (self.is_empty_leaf()):
+            raise Exception("Attempted to grow an empty leaf")
+        (img_wd, img_hgt) = sprite.get_size()
+        new_child = self.clone()
+        self.img=()
+        self.img_name=()
+        if self.rect.should_grow_vertically(img_wd,img_hgt):
+            self.children = (new_child,\
+                rect_node((), rectangle(self.rect.x, self.rect.y+self.rect.hgt, self.rect.wd, img_hgt)))
+            self.rect.hgt += img_hgt
+        else:
+            self.children= (new_child,\
+                rect_node((), rectangle(self.rect.x+self.rect.wd, self.rect.y, img_wd, self.rect.hgt)))
+            self.rect.wd += img_wd
+        self.children[1].split_node(sprite)
+
+    def to_string(self):
+        if (self.is_leaf()):
+            return "[ %s: %s ]" % (self.img_name, self.rect.to_string())
+        else:
+            return "[ %s: %s | %s %s]" % \
+                    (self.img_name, self.rect.to_string(), self.children[0].to_string(), self.children[1].to_string())
+
+    def render(self, img):
+        if (self.is_leaf()):
+            if (self.sprite):
+                pad = self.sprite.padding
+                img.paste(self.sprite.image, (self.rect.x + pad, self.rect.y + pad))
+        else:
+            self.children[0].render(img)
+            self.children[1].render(img)
+
+#----------------------------------------------------------------------
+
+def find_empty_leaf(node, sprite):
+    (img_wd, img_hgt) = sprite.get_size()
+    if (node.is_empty_leaf()):
+        return node if node.rect.can_contain(img_wd, img_hgt) else ()
+    else:
+        if (node.is_leaf()):
+            return ()
+        leaf = find_empty_leaf(node.children[0], sprite)
+        if (leaf):
+            return leaf
+        else:
+            return find_empty_leaf(node.children[1], sprite)
+
+def pack_images( sprites, grow_mode, max_dim):
+    root=()
+    while sprites:
+        sprite = sprites.pop()
+        if not root:
+            if (grow_mode):
+                (wd,hgt) = sprite.get_size()
+                root = rect_node((), rectangle(0, 0, wd, hgt))
+            else:
+                root = rect_node((), rectangle(0, 0, max_dim[0], max_dim[1]))
+            root.split_node(sprite)
+            continue
+        leaf = find_empty_leaf(root, sprite)
+        if (leaf):
+            leaf.split_node(sprite)
+        else:
+            if (grow_mode):
+                root.grow_node(sprite)
+            else:
+                raise Exception("Can't pack images into a %d by %d rectangle." % max_dim)
+    return root
+
+def nearest_power_of_two(n):
+    #there's probably some cleverer way to do this... but take the log base-2,
+    #and raise 2 to the power of the next integer...
+    log_2 = log(n) / log(2)
+    return int(2**(ceil(log_2)))
+
+def flatten_nodes(node):
+    if (node.is_leaf()):
+        if (node.sprite):
+            return [node]
+        else:
+            return ()
+    else:
+        left = flatten_nodes(node.children[0])
+        right = flatten_nodes(node.children[1])
+        if (left and not right):
+            return left
+        if (right and not left):
+            return right
+        if (left and right):
+            return left + right
+        else:
+            return ()
+
+def generate_sprite_sheet_img(packing, image_filename, should_make_power_of_two):
+    sz = ()
+    if (not should_make_power_of_two):
+        sz = (packing.rect.wd, packing.rect.hgt)
+    else:
+        padded_dim = nearest_power_of_two(max(packing.rect.wd, packing.rect.hgt))
+        sz = (padded_dim, padded_dim)
+
+    sprite_sheet = Image.new('RGBA', sz )
+    packing.render(sprite_sheet)
+    sprite_sheet.save(image_filename, 'PNG')
+    return sprite_sheet
+
+def generate_sprite_sheet_metadata(packing, filename_prefix, sz, ss_format):
+    nodes = flatten_nodes(packing)
+    metadata_generator = get_spritesheet_format(ss_format, nodes, sz)
+    if (metadata_generator == None):
+        raise Exception("unknown sprite sheet format")
+    filename = filename_prefix + "." + metadata_generator.get_extension()
+    metadata_generator.write_metadata(filename)
+
+def generate_sprite_sheet(packing, dest_file_prefix, should_make_power_of_two, ss_format):
+    image_filename = dest_file_prefix + ".png"
+    img = generate_sprite_sheet_img(packing, image_filename, should_make_power_of_two)
+    generate_sprite_sheet_metadata(packing, dest_file_prefix, img.size, ss_format)
+
+def get_images(image_dir, padding):
+    images = []
+    for file in os.listdir(image_dir):
+        img = ()
+        try:
+            img = Image.open(image_dir + os.sep + file)
+        except:
+            continue
+        if (not images):
+            images = [sprite_info(file, img, padding)]
+        else:
+            images.append(sprite_info(file, img, padding))
+    return images
+
+def get_spritesheet_format(format_option, nodes, sz):
+    dict_formats = {
+        'plist' : plist_generator(nodes, sz),
+        'json' : json_generator(nodes, sz)
+    }
+    return dict_formats[format_option] if (format_option in dict_formats) else None
+
+#----------------------------------------------------------------------
+
+
+def main():
+
+    parser = OptionParser(usage="usage: %prog [options]",
+                          version="%prog 1.0")
+
+    parser.add_option("-o", "--output_filename",
+                      action="store",
+                      default="pypack_output",
+                      help="filename (minus extensions) of the two output files")
+
+    parser.add_option("-i", "--input_dir",
+                      action="store",
+                      default="",
+                      help="input directory")
+
+    parser.add_option("-m", "--mode",
+                      action="store",
+                      default="grow",
+                      help="packingmode")
+
+    parser.add_option("-p", "--padding",
+                      action="store_true",
+                      default=0,
+                      help="padding")
+
+    parser.add_option("-x", "--power_of_two",
+                      action="store_true",
+                      default=False,
+                      help="pad to nearest power of two")
+
+    parser.add_option("-f", "--format",
+                      action="store",
+                      default="plist",
+                      help="format of spritesheet metadata (currently must be plist or json)")
+
+    try:
+        (options, args) = parser.parse_args()
+
+        images = get_images(options.input_dir, options.padding)
+        sorted_images = sort_images_by_size( images )
+
+        max_dim=()
+        if (options.mode != "grow"):
+            dim_strings = options.mode.split("x")
+            if (len(dim_strings) != 2):
+                raise Exception("Invalid packing mode")
+            try:
+                max_dim = (int(dim_strings[0]), int(dim_strings[1]))
+            except ValueError:
+                raise Exception("Invalid packing mode")
+
+        image_packing = pack_images(sorted_images, not max_dim, max_dim )
+        generate_sprite_sheet(image_packing, options.output_filename, options.power_of_two, options.format)
+    except Exception as e:
+        print("\nError: %s" % e)
+        return
+
+    print("\nPacking %s successful.\nGenerated:\n   %s\n" % \
+            ( options.input_dir, options.output_filename + ".png + " + options.format + " metadata"))
+
+if __name__ == '__main__':
+    main()
